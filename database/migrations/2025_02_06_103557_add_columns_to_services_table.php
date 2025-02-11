3d8607e0e03938c0455fa1154cf5410b3232af86
karan_b_c <you@example.com>
2025-02-11 10:22:00 +0545

    removed app-zip

---
+++ database/migrations/2025_02_06_103557_add_columns_to_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->string('seo_image')->nullable();
+            $table->text('seo_description')->nullable();
+            $table->text('seo_keyword')->nullable();
+            $table->text('seo_title')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('services', function (Blueprint $table) {
+            $table->dropColumn(['seo_image', 'seo_description', 'seo_keyword', 'seo_title']);
+        });
+    }
+}
